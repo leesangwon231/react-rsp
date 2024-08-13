131b8083e75e994816f560e9cc5d9ff5711f802e
leesangwon <werggf@naver.com>
2024-08-13 16:25:13 +0900

    noona react commit 1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from "./component/box"
+import {useState} from "react";
 
+
+// 1. 박스 2개 (타이틀 사진 결과)
+// 2. 가위 바위 보 버튼이 있다
+// 3. 버튼을 클릭하면 클릭한 값이 보임
+// 4. 컴퓨터는 랜덤하게 선택이 된다
+// 5. 3,4 번의 결과를 가지고 누가 이겼는지 승패를 따진다.
+// 6. 테두리 색깔 변경 (이기면 - 초록, 지면 - 빨강 , 비기면 - 검정)
 function App() {
+    const [userSelect,setUserSelect] = useState(null);
+
+    const choice = {
+        rock : {
+            name : "rock",
+            image : "https://m.media-amazon.com/images/I/51rOMX5z40L.jpg"
+        },
+        sissor : {
+            name : "sissor",
+            image : "https://thumbs.dreamstime.com/b/sissor-2293994.jpg"
+        },
+        paper : {
+            name : "paper",
+            image : "https://spaldingeducationstore.org/cdn/shop/products/paper-spalding-vertical-paper-5-8-inch-pa1_1000x.jpg?v=1683973391"
+        }
+    }
+
+
+    const choiceRsp = (rsp) => {
+        setUserSelect(choice[rsp])
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test2
-        </a>
-      </header>
-    </div>
+      <div>
+          <div className="main">
+              <Box title="You" item = {userSelect}/>
+              {/*<Box title="computer"/>*/}
+          </div>
+          <div className={"main"}>
+              <button onClick={() => choiceRsp("sissor")}>가위</button>
+              <button onClick={() => choiceRsp("rock")}>바위</button>
+              <button onClick={() => choiceRsp("paper")}>보</button>
+          </div>
+      </div>
   );
 }
 
